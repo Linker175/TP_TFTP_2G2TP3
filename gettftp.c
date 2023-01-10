fe2db113c55d92a9b71cd3c23e881a2140f7e053
Yoann <thebaultyoann@gmail.com>
2023-01-10 09:54:19 +0100

    3.

--- gettftp.c
+++ gettftp.c
@@ -17,4 +17,24 @@ int main(int argc, char *argv[]) {
 	printf("Host : %s\n", host);
 	printf("File : %s\n", file);
 
+	struct addrinfo hints;
+	struct addrinfo *res;
+	memset(&hints, 0, sizeof(struct addrinfo)); // Initialise à 0
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_family = AF_INET;
+	hints.ai_protocol = 0;
+	
+	int en = getaddrinfo(host, "1069", &hints, &res);
+	if (en == -1) {
+		perror("getaddrinfo");
+		exit(EXIT_FAILURE);
+	}
+	
+	int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sock == -1) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	exit(EXIT_SUCCESS);
 }
